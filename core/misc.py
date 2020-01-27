3a67d4cfb8bc0ef1766b533da8699500de4a7fc7
nailer <nailer@pm.me>
2020-01-27 15:55:20 +0200

    add runner register handlers for clients

--- core/misc.py
+++ core/misc.py
@@ -1,5 +1,6 @@
 from asyncio import get_event_loop
 from importlib import import_module
+from inspect import isawaitable
 from pkgutil import walk_packages
 from typing import List, Dict
 
@@ -21,6 +22,24 @@ def load_package(package: str):
         full_name = f'{package.__name__}.{name}'
         plugin = import_module(full_name)
         plugins[name] = plugin.commands if hasattr(plugin, 'commands') else None
+        for item_name in dir(plugin):
+            item = getattr(plugin, item_name)
+            if isawaitable(item) and not item_name.startswith('_'):
+                for client in clients.values():
+                    client.add_event_handler(item)
 
         if is_pkg:
             load_package(full_name)
+
+
+async def run():
+    for client in clients.values():
+        await client.start()
+        await dp.skip_updates()
+        try:
+            await dp.start_polling(reset_webhook=True)
+        except KeyboardInterrupt:
+            await dp.stop_polling()
+            for client in clients.values():
+                await client.disconnect()
+                await dp.wait_closed()
