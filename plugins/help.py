a096075bd74fb6a39c027930a6acd77b7cb9a206
nailer <nailer@pm.me>
2020-01-27 15:09:38 +0200

    add help plugin for listing plugins and their commands

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,7 @@
 from telethon.events import register, NewMessage
 from telethon.tl.custom import Message
 
-from core.misc import commands as commands_
+from core.misc import plugins
 
 commands = ['.plugins - list plugins',
             '.help <plugin> - show plugin commands']
@@ -9,13 +9,13 @@ commands = ['.plugins - list plugins',
 
 @register(NewMessage(outgoing=True, pattern=r'\.plugins'))
 async def list_plugins(event: Message):
-    await event.edit('**Nailerine plugins:**\n\n' + '\n'.join(commands_.keys()))
+    await event.edit('**Nailerine plugins:**\n\n' + '\n'.join(plugins.keys()))
 
 
 @register(NewMessage(outgoing=True, pattern=r'\.help\s(?P<plugin>\w+)'))
 async def list_plugin_commands(event: Message):
     plugin = event.pattern_match.group('plugin')
-    if plugin in commands_:
-        await event.edit(f'**{plugin} commands:\n\n**' + '\n'.join(commands_[plugin]))
+    if plugin in plugins:
+        await event.edit(f'**{plugin} commands:\n\n**' + '\n'.join(plugins[plugin]))
     else:
         await event.edit(f"Can't find command list for plugin **{plugin}**")
